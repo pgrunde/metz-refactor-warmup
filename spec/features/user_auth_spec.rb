2e51cd5a5eda7b7b13c387b1a2611e9fea3f2dd5
EmilyPlatzer <emilyplatzer@gmail.com>
2014-07-01 21:12:01 -0600

    User can logout

--- spec/features/user_auth_spec.rb
+++ spec/features/user_auth_spec.rb
@@ -17,6 +17,20 @@ feature 'User login' do
     click_on 'Register'
     expect(page).to have_content('Albert, welcome to your Game of TenThousand')
     expect(page).to_not have_content('Register')
+    expect(page).to have_content('Log out')
+  end
+
+  scenario 'User can log out' do
+    visit '/'
+    click_on 'Register'
+    fill_in 'Username', :with => 'Albert'
+    fill_in 'Email', :with => 'albert@example.com'
+    fill_in 'Password', :with => 'thebighouse'
+    fill_in 'Password confirmation', :with => 'thebighouse'
+    click_on 'Register'
+    click_on 'Log out'
+    expect(page).to_not have_content('Albert, welcome to your Game of TenThousand')
+    expect(page).to have_content('Register')
   end
 
 end
