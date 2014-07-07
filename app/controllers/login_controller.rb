0197be0b8886ab1773eda813b92310d4025d7817
EmilyPlatzer <emilyplatzer@gmail.com>
2014-07-07 13:34:46 -0600

    Changed helper method current_user

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,7 +1,7 @@
 class LoginController < ApplicationController
 
   def destroy
-    @_current_user = session[:current_user_id] = nil
+    @current_user = session[:current_user_id] = nil
     redirect_to root_path
   end
 
