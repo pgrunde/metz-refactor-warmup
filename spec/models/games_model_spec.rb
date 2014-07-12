56693eeb72e76e041d38101ea8301da1a79f89ad
EmilyPlatzer <emilyplatzer@gmail.com>
2014-07-12 14:28:55 -0600

    Updated model test to "initialize" game with controller create method actions, user can roll mult times and score

--- spec/models/games_model_spec.rb
+++ spec/models/games_model_spec.rb
@@ -2,12 +2,20 @@ require 'spec_helper'
 
 describe 'User can play Ten Thousand' do
 
-  it 'initializes game with scores, dice available nil, no last roll' do
+  def new_game_with_defaults
     game = Game.new
-    expect(game.total_score).to eq nil
-    expect(game.current_score).to eq nil
-    expect(game.available_dice).to eq nil
-    expect(game.last_roll).to eq []
+    game.total_score = 0
+    game.current_score = 0
+    game.last_roll = game.first_roll
+    game
+  end
+
+  it 'initializes game with scores, dice available nil, no last roll' do
+    game = new_game_with_defaults
+    expect(game.total_score).to eq 0
+    expect(game.current_score).to eq 0
+    expect(game.available_dice).to eq 6
+    expect(game.last_roll.size).to eq 6
   end
 
   it 'allows a user to roll, tracks last roll' do
@@ -27,6 +35,18 @@ describe 'User can play Ten Thousand' do
     game.stay(['1', '5'])
     actual = game.total_score
     expected = 150
+    expect(actual).to eq(expected)
+    srand(Random.new_seed)
+  end
+
+  it 'allows a user to roll multiple times and then score 1s and 5s' do
+    srand(1)
+    game = new_game_with_defaults
+    game.first_roll
+    game.roll_again(['1', '1', '5'])
+    game.stay(['5', '5'])
+    actual = game.total_score
+    expected = 350
     expect(actual).to eq(expected)
     srand(Random.new_seed)
   end
